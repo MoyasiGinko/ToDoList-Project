b07b372e6b748df9143a16f35e3304c4885dcef0
Moyasi Ginko <mrsiyam61@gmail.com>
2023-05-11 08:29:25 +0600

    Add index.js file and imported files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// import _ from 'lodash';
+import './style.css';
+import './modules/function.js';
+// import { renderTask, renderForm } from './modules/data.js';
+
+// renderTask();
+// renderForm();
