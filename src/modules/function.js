ad07adcecd6f8b7ff53cd4576e2feff7c70f1c60
Moyasi Ginko <mrsiyam61@gmail.com>
2023-05-13 03:47:10 +0600

    Update function.js

--- src/modules/function.js
+++ src/modules/function.js
@@ -1,3 +1,4 @@
+// A Class for editing tasks from the task list
 class EditTask {
   static editDescription(taskIndex, newDescription, tasks, setTasks) {
     const updatedTasks = tasks.map((task, index) => {
