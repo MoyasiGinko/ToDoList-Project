a341774bea6bf312acc08b1b464af27b2c4c8d0e
Moyasi Ginko <mrsiyam61@gmail.com>
2023-05-12 23:41:30 +0600

    Add clear buttons

---
+++ src/modules/clear.js
@@ -0,0 +1,22 @@
+export const updateStorage = (tasks) => {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+export const ClearTask = (dataInstance) => {
+  const clearButton = document.querySelector('.clear-button');
+  clearButton.addEventListener('click', () => {
+    const todolist = document.querySelector('.todoLists');
+    const completedTasks = todolist.querySelectorAll('.completed');
+
+    // remove only completed tasks if there are any
+    if (completedTasks.length > 0) {
+      const tasks = dataInstance.getTasks().filter((task) => !task.completed);
+      dataInstance.setTasks(tasks);
+    }
+
+    // update storage with the latest tasks
+    updateStorage(dataInstance.getTasks());
+
+    dataInstance.pendingTasks();
+  });
+};
