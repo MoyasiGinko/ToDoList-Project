c337510d78074403387d9ac933a719234f9a1415
Moyasi Ginko <mrsiyam61@gmail.com>
2023-05-20 01:52:14 +0600

    Update file

--- src/__test__/todo-list.test.js
+++ src/__test__/todo-list.test.js
@@ -1,5 +1,6 @@
 import { deleteData, addTask } from '../modules/taskUtils.js';
 import EditTask from '../modules/function.js';
+import CompletedTask from '../modules/completed.js';
 import { set } from 'lodash';
 
 describe('deleteData function', () => {
@@ -116,7 +117,26 @@ describe('editTask function', () => {
       newDescription,
       tasks,
       setTasks,
-      updateStorage,
+      updateStorage
+    );
+
+    expect(setTasks).toHaveBeenCalledWith(expectedTasks);
+  });
+
+  test('should mark tasks to completed', () => {
+    taskIndex = 1;
+
+    const expectedTasks = [
+      { description: 'Task 1', completed: false, index: 1 },
+      { description: 'Task 2', completed: true, index: 2 },
+    ];
+
+    CompletedTask.markComplete(
+      taskIndex,
+      { completed: true },
+      tasks,
+      setTasks,
+      updateStorage
     );
 
     expect(setTasks).toHaveBeenCalledWith(expectedTasks);
